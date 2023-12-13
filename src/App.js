f6660f1cebbd5915912e55cef772e3e5604746f0
Shuja ud Din <shuja1339@gmail.com>
2023-12-13 18:09:22 +0500

    helmet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css"
 import Home from "./pages/Home";
+import { Helmet } from "react-helmet";
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Helmet>
+          <title>Testador de teclado - Teclas de teclado de teste on-line</title>
+          <meta name="description"
+            content="Teste de Teclado é uma ferramenta de diagnóstico projetada para avaliar e solucionar problemas de funcionalidade e desempenho do teclado."
+          />
+          <link rel="canonical" href="/" />
+        </Helmet>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
