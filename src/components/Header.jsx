efa7eb9aaadf4a27ce59fdacefb07d9704b44264
Shuja ud Din <shuja1339@gmail.com>
2023-12-13 15:50:21 +0500

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../assets/images/header-logo.svg"
+import logo from "../assets/images/header-logo.png"
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
